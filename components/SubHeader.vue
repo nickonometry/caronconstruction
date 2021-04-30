99c56ec8429427b5d7e99f68248c753985048cfa
nick.andrews <nick.andrews@tylertech.com>
2021-04-30 12:27:59 -0400

    added subheadercomponent

---
+++ components/SubHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="sub-header"
+    :style="{
+      backgroundImage:
+        'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(' +
+        image +
+        ')',
+    }"
+  >
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    title: String,
+  },
+}
+</script>
+
+<style scoped>
+.sub-header {
+  font-family: 'Bebas Neue', cursive;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 140px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8));
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  color: white;
+  font-size: 2rem;
+  z-index: 10;
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <!-- <SubHeader
+    <SubHeader
       title="About Us"
       image="https://images.unsplash.com/photo-1601284953533-6cadbbebb347?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-    /> -->
+    />
     <div class="page-container">
       <section class="two-column">
         <h3 class="span-full">Who We Are</h3>
