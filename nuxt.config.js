42e562680210dca1da50cd55b041efb2de64efc9
nick.andrews <nick.andrews@tylertech.com>
2021-04-30 12:24:14 -0400

    added global css

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/main.css',
+    '@/assets/css/mixins.scss',
+    '@/assets/css/variables.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
