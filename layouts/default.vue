1165bc5d8c733c2e112c1cfac83b561a7ee4f133
nick.andrews <nick.andrews@tylertech.com>
2021-05-03 09:23:49 -0400

    added to layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,15 @@
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  head() {
-    return {
-      script: [
-        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
-      ],
-    }
-  },
-}
-</script>
-
 <style>
 .container {
   margin: 0 auto;
